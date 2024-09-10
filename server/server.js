8e296c4f10f474893546074b02f218b4b155812a
Dev22Patel <156922885+Dev22Patel@users.noreply.github.com>
2024-09-11 00:31:40 +0530

    updated the backend

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 const router = require("./router/auth-router")
 const connectDb = require("./utils/db");
 const PORT=3000;
+const eventHostRoutes = require('./router/eventHostRoutes');
+const eventRoutes = require('./router/eventRoutes');
+const auctionRoutes = require('./router/auctionRoutes');
+const sponsorRoutes = require('./router/sponsorRoutes');
 
 connectDb().then(()=>{
     app.listen(PORT, () => {
@@ -10,4 +14,10 @@ connectDb().then(()=>{
       });
 })
 
-app.use("/api/auth",router);
+app.use(express.json()); // For parsing application/json
+
+// Routes
+app.use('/api/eventhosts', eventHostRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/auctions', auctionRoutes);
+app.use('/api/sponsors', sponsorRoutes);
